507a4b3fca855155d30d81e3623dd401a5aa52be
sai-vl90 <saisupriya.vadali@bs.nttdata.com>
2025-01-16 11:36:32 +0530

    Commit 17

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 python -m pip install --upgrade pip setuptools wheel
-pip install --force-reinstall -r requirements.txt
+pip uninstall -y azure-core azure-identity
+pip install --upgrade --force-reinstall -r requirements.txt

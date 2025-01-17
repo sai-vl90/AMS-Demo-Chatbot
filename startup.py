912b10e96c9266239ff89f1ad5ac53ec61bc40c2
sai-vl90 <saisupriya.vadali@bs.nttdata.com>
2025-01-17 15:51:19 +0530

    Commit 2

--- startup.py
+++ startup.py
@@ -1,5 +1,4 @@
-from waitress import serve
 from app import application
 
 if __name__ == '__main__':
-    serve(application, host='0.0.0.0', port=8000)
+    application.run(host='0.0.0.0', port=8000)

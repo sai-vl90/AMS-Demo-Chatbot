c833b8bbf1b8a9ba620f42d3c4d54773ead765ad
sai-vl90 <saisupriya.vadali@bs.nttdata.com>
2025-01-22 12:37:09 +0530

    Commit 6

--- custom_startup.sh
+++ custom_startup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+apt-get update
+apt-get install  -y  libgl1-mesa-glx  libglib2.0-0
+pip install  -r  requirements.txt
 
 # 1) Remove /agents/python from PYTHONPATH
 #    or simply replace the entire PYTHONPATH to point only to your venv
@@ -8,6 +11,7 @@ export PYTHONPATH=$(echo "$PYTHONPATH" | sed 's/\/agents\/python://')
 
 # Approach B: Or just forcibly set PYTHONPATH to your venv site-packages:
 # export PYTHONPATH=/home/site/wwwroot/antenv/lib/python3.12/site-packages
+
 
 # 2) Launch your app with Waitress
 python startup.py
